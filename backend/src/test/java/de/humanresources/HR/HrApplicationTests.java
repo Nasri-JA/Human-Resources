1b791e544bfafb1c6375803bc1c07749770f35f8
Nasri <120532976+Nasri-JA@users.noreply.github.com>
2024-03-20 01:42:10 +0100

    Add Spring

---
+++ backend/src/test/java/de/humanresources/HR/HrApplicationTests.java
@@ -0,0 +1,13 @@
+package de.humanresources.HR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
